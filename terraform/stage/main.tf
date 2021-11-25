3cd43a26b3b0244665b7c6a0b69087a25c24aa3f
zmd73 <vlad1973@yandex.ru>
2021-11-25 04:56:08 +0300

    change file example

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,5 +1,4 @@
 provider "yandex" {
-  version                  = "~> 0.64.1"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
