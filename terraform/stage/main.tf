707eb5386b1482215b87784ee51cf743c82619cd
zmd73 <vlad1973@yandex.ru>
2021-11-25 05:36:41 +0300

    add files ansible-1

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,4 +1,5 @@
 provider "yandex" {
+  version                  = "0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
